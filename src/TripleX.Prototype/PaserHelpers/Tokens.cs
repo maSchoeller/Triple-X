23935dbe4a48cbc9af00bf1949b4fe38508d3fc6
maSchoeller <git@marvinschoeller.de>
2021-04-20 16:58:06 +0200

    [dev]: starting working on parser

---
+++ src/TripleX.Prototype/PaserHelpers/Tokens.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TripleX.Prototype.PaserHelpers
+{
+    public record BaseToken();
+
+    public record NumberToken(int Number) : BaseToken;
+    //public record WhitespaceToken() : BaseToken;
+    public record SeperatorToken(char Seperator) : BaseToken;
+    public record PlusToken() : BaseToken;
+    public record OpenBracketToken(char Bracket) : BaseToken;
+    public record CloseBracketToken(char Bracket) : BaseToken;
+    public record InvalidToken() : BaseToken;
+    public record EndOfFileToken() : BaseToken;
+
+
+    public record TokenGroup() : BaseToken;
+
+    public record NumberGroup(IList<BaseToken> Childs) : TokenGroup;
+    public record BracketGroup(IList<BaseToken> Childs, OpenBracketToken Open, CloseBracketToken Close): TokenGroup;
+
+    public record PhoneNumber(TokenGroup Country, TokenGroup Area, TokenGroup Main, TokenGroup Forwarding);
+}
--- src/TripleX.Prototype/PhonenumberParser.cs
+++ src/TripleX.Prototype/PhonenumberParser.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using TripleX.Prototype.PaserHelpers;
 
 namespace TripleX.Prototype
 {
@@ -15,7 +15,116 @@ namespace TripleX.Prototype
 
         public (string Country, string Area, string Main, string Forwarding, string ErrorMessage) Parser(string number)
         {
+
+            number = number.Replace(" ", "");
+            var tokens = CreateToken(number).ToList();
+
+
+
             return ("+49", "7443", "2899033", "235", "Kein error");
+        }
+
+
+
+        private PhoneNumber CreatePhonenumber(IList<BaseToken> tokens)
+        {
+            var current = 0;
+            BaseToken currentToken = Peek();
+            TokenGroup country;
+            TokenGroup area;
+            TokenGroup main;
+            TokenGroup forward;
+
+
+            //Get CountryCode
+            if (currentToken is PlusToken)
+            {
+                //Note: Usecase +<two digits countrycode>, e.g. +49, +31
+                var number1 = NextToken();
+                var number2 = NextToken();
+                if (number1 is NumberToken && number2 is NumberToken)
+                {
+                    //TODO: Catch  usecase [+49]
+                    country = new NumberGroup(new BaseToken[] { currentToken, number1, number2 });
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+            else if (currentToken is NumberToken { Number: 0 })
+            {
+                //Note: Usecase 00<two digits countrycode>, e.g.: 0049, 0031
+                var nexttoken = NextToken();
+                if (nexttoken is NumberToken { Number: 0 })
+                {
+                    var number1 = NextToken();
+                    var number2 = NextToken();
+                    if (number1 is NumberToken && number2 is NumberToken)
+                    {
+                        //TODO: Catch  usecase [0049]
+                        country = new NumberGroup(new BaseToken[] { currentToken, number1, number2 });
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+            }
+            currentToken = NextToken();
+            if (currentToken is NumberToken)
+            {
+                //TODO: Area code 
+            }
+
+
+
+            BaseToken NextToken()
+            {
+                current++;
+                return Peek();
+            }
+
+            BaseToken Peek(int offset = 0)
+            {
+                if (tokens.Count > current + offset)
+                {
+                    return new InvalidToken();
+                }
+                return tokens[current + offset];
+            }
+            return new PhoneNumber(country, area, main, forward);
+        }
+
+
+        private IEnumerable<BaseToken> CreateToken(string phoneNumber)
+        {
+            foreach (var character in phoneNumber)
+            {
+                if (character == '+')
+                {
+                    yield return new PlusToken();
+                }
+                if (char.IsDigit(character))
+                {
+                    yield return new NumberToken(Convert.ToInt32(character));
+                }
+                if (character is '/' or '-')
+                {
+                    yield return new SeperatorToken(character);
+                }
+                if (character is '(' or '[')
+                {
+                    yield return new OpenBracketToken(character);
+                }
+                if (character is ')' or ']')
+                {
+                    yield return new CloseBracketToken(character);
+                }
+                yield return new InvalidToken();
+            }
+
+            yield return new EndOfFileToken();
         }
     }
 }
